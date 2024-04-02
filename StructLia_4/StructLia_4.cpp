d955ca94cebf977836c58a7892cd801c6ef2e7c6
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-04-02 11:39:45 +0700

    Menambahkan [i] pada tiap mhs

--- StructLia_4/StructLia_4.cpp
+++ StructLia_4/StructLia_4.cpp
@@ -20,4 +20,8 @@ int main() {
     Mahasiswa mhs[3];
     for (int i = 0; i < 3; i++) {
         cout << "Data ke-" << (i + 1) << " : " << endl;
+        cout << "Nomor Mahasiswa : ";
+        getline(cin, mhs[i].nim);
+        cout << "Nama Mahasiswa : ";
+        getline(cin, mhs[i].nama);
 }
