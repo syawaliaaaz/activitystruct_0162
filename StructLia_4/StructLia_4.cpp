16c9372272c2ada7b06559ed1d0316e7e5480e5e
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-04-02 11:49:05 +0700

    Membuat Line cout dan memasukkan cout semua data yang sudah diinput diatasnya. selesai.

--- StructLia_4/StructLia_4.cpp
+++ StructLia_4/StructLia_4.cpp
@@ -37,3 +37,13 @@ int main() {
     }
 
     for (int i = 0; i < 3; i++) {
+        cout << endl;
+        cout << "Data Mahasiswa ke-" << (i + 1) << " : " << endl;
+        cout << "\n NIM : " << mhs[i].nim;
+        cout << "\n Nama : " << mhs[i].nama;
+        cout << "\n Alamat : ";
+        cout << "\n \t Desa : " << mhs[i].alamat.desa;
+        cout << "\n \t Kota : " << mhs[i].alamat.kota;
+        cout << "\n Umur : " << mhs[i].umur;
+    }
+}
